621f84b5a7b7b9f3179840c2f2e6fa4314d68c68
emilliman5 <emilliman5@users.noreply.github.com>
2014-08-14 11:28:25 -0400

    new script for mapping primer pairs
    Script to match primer pairs from PCRtiler output. Creates a bed file
    for viewing a genome snapshot in IGV

---
+++ primer_toBed.pl
@@ -0,0 +1,60 @@
+#!/usr/local/bin/perl
+
+#script that will map oligonucleotide sequences (specifically the output from PCRtiler) and output bed file. Ideally used for small genome snapshots on IGV.
+
+#08/14/2014 EJM
+
+use strict;
+
+open (MMTV, "<$ARGV[0]") || "Could not open: $ARGV[0]\n"; #input sequence
+
+open(PRIMERS, "<$ARGV[1]")  || "Could not open: $ARGV[0]\n"; #primer sequences
+
+open (BED, ">>$ARGV[2]"); #output file
+
+my $genome_seq="";
+my $seqID='';
+
+while(<MMTV>){
+
+	chomp;
+	if(/^>/){
+	
+		$seqID=substr($_,1);
+		}
+	
+	else{
+		
+		$genome_seq .=$_;
+		
+		}
+	
+	}
+	
+print BED "#ggfTags\n";
+
+while(<PRIMERS>){
+	
+	next if (!/^\d/);
+	chomp;
+	my $left='';
+	my $right='';
+	
+	my @line=split("\t",$_);
+	
+		if($genome_seq=~/($line[1])/){
+			#print "Found a match\n";
+			$left=$-[0];
+		}
+		
+		my $rc=reverse($line[2]);
+		$rc=~tr/AGTC/TCAG/;
+			
+		if($genome_seq=~/($rc)/){
+			$right=$+[0];
+			}
+	
+	print BED "$seqID\t".$left."\t".$right."\tPrimer_".$line[0]."\t1\t.\n";
+	}
+	
+		
