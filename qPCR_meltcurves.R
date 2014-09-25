8bb07a850f16f69d6590d0e6592f5e80b1a63903
emilliman5 <emilliman5@users.noreply.github.com>
2014-09-25 18:00:41 -0400

    changed primer template commands
    changed the way plate loading template is handled.

--- qPCR_meltcurves.R
+++ qPCR_meltcurves.R
@@ -18,13 +18,16 @@ qt<-melt(qpcr.table, id = "Temperature", variable.name="Well", value.name="dRFU"
 qt$row<-as.factor(substring(as.character(qt$Well), 0, 1))
 qt$column<-as.factor(substring(as.character(qt$Well), 2))
 
-primer<-read.xlsx2(dir(pattern="plate"), sheetIndex = 4, as.data.frame = TRUE, header=FALSE)
+primer<-read.xlsx2(dir(pattern="plate"), sheetIndex = 1, as.data.frame = TRUE, header=FALSE)
 
-primer$X2<-gsub("/", "-", primer$X2)
+primer.list<-primer[2,2:25]
+primer.list[1,]<-gsub("/", "-", primer.list[1,])
+
+
 
 for (i in levels(qt$column)) {
   
-  png(paste("meltCurve_2step_", primer[as.numeric(i)], ".png", sep =""), height=800, width=1200, units="px")
+  png(paste("meltCurve_2step_", primer.list[,as.numeric(i)], ".png", sep =""), height=800, width=1200, units="px")
   p<-ggplot(qt[qt$column==i,], aes(Temperature, dRFU, col=row)) + geom_line(size=1) + labs(title=primer[as.numeric(i),2])
   print(p)
   dev.off()
