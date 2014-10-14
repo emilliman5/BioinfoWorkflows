7ade180f6bb6eb62bb759553886d34b75b4ec6a3
emilliman5 <emilliman5@users.noreply.github.com>
2014-10-14 17:04:22 -0400

    added argument for fragment length split point. started to multi thread script for faster execution.

--- sam_intrapairDist_thread.pl
+++ sam_intrapairDist_thread.pl
@@ -3,6 +3,9 @@
 
 use strict;
 use File::Basename;
+use threads;
+
+my $number_of_threads=4;
 
 my @suffixlist=(".sam", ".bam", ".bam.bai", ".fastq");
 my $frag=$ARGV[0];
@@ -19,28 +22,41 @@ open (DIST, ">>$ARGV[4]");
 
 while (<SAM>){
 
-	chomp;
-	
+	chomp;	
 	if( $_=~/^\@/){
 		print NFR $_."\n";
 		print NUC $_."\n";
 		}
 	else {
 	
-		my @tmp=split(/\t/, $_);
+		
+	}
+	
+sub initThreads{
+	my  @initThreads;
+		foreach(my $i=1; $i<=$number_of_threads; $i++){
+			push(@initThreads, $i);
+			}
+		return @initThreads;
+		}
+	
+sub splitSam{
+	
+	my $id =threads->tid();
+	my $line=shift;
+	
+	my @tmp=split(/\t/, $line);
 		
 		if ($tmp[8] >=0 ){
 			print DIST $tmp[8]."\n";
 		}
 		
 		if (abs($tmp[8]) <= $frag){
-	
-			print NFR $_."\n";
+			print NFR $line."\n";
 			}
 			
-		else {
-			
-			print NUC $_."\n";
+		else {			
+			print NUC $line."\n";
 			}
+		threads->exit();
 		}
-	}
