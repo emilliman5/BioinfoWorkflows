fc614671a7a5c745131f3741f8c60ce7b552f1fe
emilliman5 <emilliman4@gmail.com>
2015-01-10 14:32:46 -0500

    added plan to complete script

--- batchHeatmapper.py
+++ batchHeatmapper.py
@@ -8,9 +8,24 @@
 #pass through all other options
 #	
 #determine the Zmax value and apply to all plots
-#sort all files with the same list if specified or specified func.
+#sort all files with the same regions file, same func. or sort one plot and sort all others based on first plot.
 #determine the scaling for each plot based on matrix with largest number of regions (heatmaps only)
-#	--long term: allow the passing of feature line width in px for heatmaps
+#	--long term: allow the passing of feature line width in px for heatmap rows
 #
 #call heatmapper or profiler and render plots (parallelize)
 #put all plots into a new subdirectory
+
+# 1.    Write a python script that executes heatmapper
+# 2.    Write a python script that will execute heatmapper over a list of files
+# 3.    Write a python script that will take arguments to pass to heatmapper
+# 4.    Write a python script that will extract values from heatmapper before executing
+
+    
+
+
+import argparse
+
+from deeptools import parserCommon ## contains all of the option flags for heatmapper and profiler
+from deeptools import heatmapper    ## should be able to classes/methods to determine some parameters
+
+
