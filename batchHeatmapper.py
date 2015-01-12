7d33f7b2ccf3a6fbb10407aa2847fee365bcd63c
emilliman5 <emilliman4@gmail.com>
2015-01-12 18:21:14 -0500

    script broken. Need to fix parameter passing into heatmapper universe

--- batchHeatmapper.py
+++ batchHeatmapper.py
@@ -15,17 +15,27 @@
 #call heatmapper or profiler and render plots (parallelize)
 #put all plots into a new subdirectory
 
-# 1.    Write a python script that executes heatmapper
+# 1.    Write a python script that executes heatmapper/profiler
 # 2.    Write a python script that will execute heatmapper over a list of files
 # 3.    Write a python script that will take arguments to pass to heatmapper
 # 4.    Write a python script that will extract values from heatmapper before executing
 
     
-
-
-import argparse
-
+from sys import argv
+import imp
 from deeptools import parserCommon ## contains all of the option flags for heatmapper and profiler
 from deeptools import heatmapper    ## should be able to classes/methods to determine some parameters
+hmScript=imp.load_source('hmScript', '/home/millimanej/workspace/deepTools/bin/heatmapper')
+
+script, input_file=argv
+zMx=10
+
+#for i in range(input_file):
+    
+args=hmScript.parseArguments(matrixfile=input_file, zMax=zMx, outfile="test_out.png")
+hmScript.main(args)
+
+
+print "I have run the deeptools heatmapper script from my batch script"
 
 
