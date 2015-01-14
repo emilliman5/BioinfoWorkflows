8f85bec158eba9e335401bc53a5c1963e071b16e
emilliman5 <emilliman4@gmail.com>
2015-01-14 13:45:15 -0500

    rewrote ToDo. Added multiprocessing. Added direct passthrough into heatmapper

--- batchHeatmapper.py
+++ batchHeatmapper.py
@@ -1,14 +1,10 @@
-#python wrapper for deeptools heatmapper and profiler
+#python wrapper for deeptools heatmapper (profiler will be next)
 #
-#inputs:	matrix files (req)
-#	        output file basename (req)
-#	        sort function (regions file not needed because the matrices should be all different regions)
-#	        plot size H & W (req)
-#
-#               pass through all other options
-#
-#
-# 4.    Write a python script that will extract intensity values from heatmapper before executing
+#ToDo:  Sanity check direct-passthrough arguments to heatmapper (everything captured by -hf)
+#       Test multithreading, conflicting arguments got me stuck in an endless array of child processes
+#       Add heatmap scaling: i.e. if region_set1  is 2000 features and region_set 2 is 1000 features the heatmap
+#           of region_set2 should be half the size of regioin_set1
+#       Dynamically extract highest intensity values (zMax) to be used across all matrix files
 
 
 import argparse
@@ -20,22 +16,32 @@ hmScript=imp.load_source('hmScript', '/home/millimanej/workspace/deepTools/bin/h
 
 parser=argparse.ArgumentParser(description="Batch process matrix files into heatmaps using deepTools heatmapper")
 parser.add_argument("-f","--files", nargs='+', help="list of matrices to be processed in batch")
-parser.add_argument("-zMx", type=float, help="Maximum value for heatmap intensities (for all matrices)", default=0)
+parser.add_argument("--zMax", type=float, help="Maximum value for heatmap intensities (for all matrices)", default=0)
 parser.add_argument("--ext", choices=["png","pdf","eps","svg","emf"], default="png", help="Specifies filetype for heatmap image")
 parser.add_argument("--prefix", default="", help="string to prepend outfile filenames")
 parser.add_argument("--suffix", default="", help="string to append to outfile names")
 parser.add_argument("-hh", action="store_true", help="Equivalent to: heatmapper --help")
 parser.add_argument("-p",type=int, help="Number of processers to disrisbute heatmapper across", default=1)
-parser.add_argument("-R", "--regions", help="Bed file to sort each heatmap by")
+parser.add_argument('-hf', help="Extra heatmapper flags: string of flags and values to be passed "
+                    "directly to the heatmapper script for all matrix files; Must be last argument"
+                    "on command-line. WARNING: There is no sanity check for the flags passed directly to heatmapper"
+                    , nargs=argparse.REMAINDER) ##Hacky pass-through of arguments into heatmapper; there is currently
+                    #no sanity check and conflicting parameters causes the script to hang.
+
 
 batch_args=parser.parse_args()
 
 if batch_args.hh:
+    parser.print_help()
+    print "\n\n"
     args = hmScript.parseArguments(['--help'])
     hmScript.main(args)
+    exit
 
-for f in batch_args.files:
+files=batch_args.files
 
+def heatmap(f):
+    
     outfile = os.path.splitext(f)[0]
     if batch_args.suffix:
         outfile=outfile+"."+batch_args.suffix
@@ -44,18 +50,25 @@ for f in batch_args.files:
         outfile=batch_args.prefix+outfile
 
     outfile=outfile+"."+batch_args.ext
+    
+    if batch_args.hf:
+        argList=batch_args.hf+['-m',f,'-o',outfile]
+        args = hmScript.parseArguments(argList)
+    else:
+        args = hmScript.parseArguments(['-m', f,'-o', outfile])
 
-    args = hmScript.parseArguments(['-m', f,'-o', outfile])
-
-    if batch_args.zMx:
-        args.zMax=batch_args.zMx
-        args.yMax=batch_args.zMx ## the profile atop the heatmaps should be on the same scale as the heatmap intenisites
-
-    if batch_args.regions:
-        args.
+    if batch_args.zMax:
+        args.zMax=batch_args.zMax
+        args.yMax=batch_args.zMax ## the profile atop the heatmaps should be on the same scale as the heatmap intenisites
     
     hmScript.main(args)
 
+def mp_handler():
+    p=multiprocessing.Pool(batch_args.p)
+    p.map(heatmap, files)
+    
+if __name__ == '__main__':
+    mp_handler()
 
 
 
