d97e3a91e20483c928cbdd20427a508cff619f80
emilliman5 <emilliman4@gmail.com>
2015-01-15 18:28:05 -0500

    broken.... commented out all dynamic calculations. something wrong with module import

--- batchHeatmapper.py
+++ batchHeatmapper.py
@@ -5,15 +5,13 @@
 #       Add heatmap scaling: i.e. if region_set1  is 2000 features and region_set 2 is 1000 features the heatmap
 #           of region_set2 should be half the size of regioin_set1
 #       Dynamically extract highest intensity values (zMax) to be used across all matrix files
-
-
 import argparse
 import imp
 import os
 import gzip
 import multiprocessing
-from numpy import percentile, concatenate
-from deeptools import parserCommon ## contains many of the option flags for heatmapper and profiler
+#from numpy import percentile, concatenate
+#from deeptools import parserCommon ## contains many of the option flags for heatmapper and profiler
 hmScript=imp.load_source('hmScript', '/home/millimanej/workspace/deepTools/bin/heatmapper')
 
 parser=argparse.ArgumentParser(description="Batch process matrix files into heatmaps using deepTools heatmapper")
@@ -29,7 +27,7 @@ parser.add_argument('-hf', help="Extra heatmapper flags: string of flags and val
                     "directly to the heatmapper script for all matrix files; Must be last argument"
                     "on command-line. WARNING: There is no sanity check for the flags passed directly to heatmapper"
                     , nargs=argparse.REMAINDER) ##Hacky pass-through of arguments into heatmapper; there is currently
-                    #no sanity check and conflicting parameters causes the script to hang.
+                    #no sanity check and conflicting parameters causes problems with the multiprocessing.
 
 
 batch_args=parser.parse_args()
@@ -40,19 +38,20 @@ if batch_args.hh:
     args = hmScript.parseArguments(['--help'])
     hmScript.main(args)
     exit
-zMax= 1 if batch_args.zMax else 0
+#zMax= 1 if batch_args.zMax else 0
 files=batch_args.files
 longest=0
 lines={}
 
-def z_values(matrixDict):
-    matrixFlatten = numpy.concatenate([x for x in matrixDict.values()]).flatten()
-    if batch_args.zMax < numpy.percentile(matrixFlatten, 98.0):
-        batch_args.zMax = numpy.percentile(matrixFlatten, 98.0)
-    if batch_args.zMin < numpy.percentile(matrixFlatten, 1.0):
-        batch_args.zMin = numpy.percentile(matrixFlatten, 1.0)
-    matrixFlatten[numpy.isnan(matrixFlatten) == False]
-    return
+#def z_values(matrixDict):
+#    matrixFlatten = numpy.concatenate([x for x in matrixDict.values()]).flatten()
+#    if batch_args.zMax < numpy.percentile(matrixFlatten, 98.0):
+#        batch_args.zMax = numpy.percentile(matrixFlatten, 98.0)
+#    if batch_args.zMin < numpy.percentile(matrixFlatten, 1.0):
+#        batch_args.zMin = numpy.percentile(matrixFlatten, 1.0)
+#    matrixFlatten[numpy.isnan(matrixFlatten) == False]
+#    matrixFlatten=''
+#    return
 
 def heatmap(f):
     outfile = os.path.splitext(f)[0]
@@ -86,13 +85,13 @@ def mp_handler():
     p=multiprocessing.Pool(batch_args.p)
     p.map(heatmap, files)
     
-for f in files:
-    with gzip.open(f, 'rb') as infile:
-        content=infile.read()
-    longest=len(content) if len(content) > longest else longest
-    lines[f]=len(content)
-    if not zMax:
-      z_vaules(content)
+#for f in files:
+#    with gzip.open(f, 'rb') as infile:
+#         content= [line.strip().split("\t") for line in f.readlines()]
+#    longest=len(content) if len(content) > longest else longest
+#    lines[f]=len(content)
+#    #if not zMax:
+#    #  z_vaules(content)
  
 if __name__ == '__main__':
     mp_handler()
