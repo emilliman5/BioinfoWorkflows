2477a37dd71216c702b036c6f5857f8ab7adae5e
emilliman5 <emilliman4@gmail.com>
2015-01-28 18:44:03 -0500

    tweaked string parsing, still non-functioning

--- unique_read_filter.py
+++ unique_read_filter.py
@@ -9,18 +9,18 @@ last_read = None
 XS=0
 for line in f :
     #take care of the header
-    if(line[0][0] == "@") :
+    if(line[0][0] == "@"):
         of.writerow(line)
         continue
 
-    if(last_read == None) : 
+    if(last_read == None): 
         last_read = line
-        if(re.split('\t|:',line)[20] =="XS" & re.split('\t|:',line)[20] <= re.split('\t|:',line)[23]):
+        if(re.split(':',line[13])[0] == "XS" & re.split(':',line[12])[2] == re.split(':',line[13])[2]):
             XS=1
         
     else :
         if(last_read[0] == line[0]):    
-            if(XS==1 & re.split('\t|:',line)[20] =="XS" & XS==0 & re.split('\t|:',line)[20] <= re.split('\t|:',line)[23]):
+            if(XS==1 & re.split(':',line[13])[0] =="XS" & XS==0 & re.split(':',line[12])[2] == re.split(':',line[13])[2]):
                 last_read = None
                 XS=0 
                 
@@ -32,5 +32,5 @@ for line in f :
         else :
             last_read = line
             XS=0
-            if(re.split('\t|:',line)[20] =="XS" & re.split('\t|:',line)[20] <= re.split('\t|:',line)[23]):
+            if(re.split(':',line[13])[0] == "XS" & re.split(':',line[12])[2] == re.split(':',line[13])[2]):
                 XS=1
