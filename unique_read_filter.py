2351dc203a3388dabff081f60aa8f1cf5e3484c0
emilliman5 <emilliman4@gmail.com>
2015-01-29 12:26:05 -0500

    fixed strig parsing and conditional tests

--- unique_read_filter.py
+++ unique_read_filter.py
@@ -5,9 +5,11 @@ import re
 
 f = csv.reader(sys.stdin, dialect="excel-tab")
 of = csv.writer(sys.stdout, dialect="excel-tab")
+
 last_read = None
 XS=0
-for line in f :
+
+for line in f:
     #take care of the header
     if(line[0][0] == "@"):
         of.writerow(line)
@@ -15,15 +17,16 @@ for line in f :
 
     if(last_read == None): 
         last_read = line
-        if(re.split(':',line[13])[0] == "XS" and int(re.split(':',line[12])[2]) == int(re.split(':',line[13])[2])):
+        
+        if(re.split(':',line[12])[0] == "XS" and int(re.split(':',line[12])[2]) == int(re.split(':',line[12])[2])):
             XS=1
         
     else :
         if(last_read[0] == line[0]):    
-            if(XS==1 and re.split(':',line[13])[0] =="XS" and XS==0 & int(re.split(':',line[12])[2]) == int(re.split(':',line[13])[2])):
+            if(XS==1 and re.split(':',line[12])[0] =="XS" and int(re.split(':',line[12])[2]) == int(re.split(':',line[12])[2])):
                 last_read = None
-                XS=0 
-                
+                XS=0
+ 
             else:
                  of.writerow(last_read)
                  of.writerow(line)
