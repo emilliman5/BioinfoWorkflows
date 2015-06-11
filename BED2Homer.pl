63571ad115a0346201b1d7ba2226fcc24cf5ba1c
emilliman5 <emilliman4@gmail.com>
2015-06-11 17:34:50 -0400

    minor edit

--- BED2Homer.pl
+++ BED2Homer.pl
@@ -6,11 +6,9 @@ open (INFILE, "<$ARGV[0]") || die "Could not open $ARGV[0]\n";
 open (OUTFILE, ">>$ARGV[1]");
 
 while (<INFILE>){
-
 	chomp;
 	
 	my @line=split("\t", $_);
 	my $exp=2**$line[4];
 	print OUTFILE join("\t", @line[0..2], $exp, "+\n");
-
 	}
